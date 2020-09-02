538668bc47b030f9f935831884963ba05e0ed219
TylerJM1127 <tjmundel@iu.edu>
2020-09-02 11:37:08 -0400

    Lab 1

---
+++ labs/lab1/app.js
@@ -0,0 +1,18 @@
+function setup() {
+    var red = "#FF0000";
+    var startX = 400;
+    createCanvas(400, 400);
+    background(red);
+    background(220);
+    fill(0, 0, 0);
+    stroke(0, 0, 255);
+    strokeWeight(20);
+    ellipse(130, 90, 80, 80);
+    ellipse(260, 90, 80, 80);
+    fill(255, 255, 255);
+    stroke(255, 255, 255);
+    rect(72, 200, 250, 60);
+    fill(0, 0, 0);
+    stroke(0, 0, 0);
+    rect(92, 210, 210, 40);
+  }
